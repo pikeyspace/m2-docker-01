8bc0d3decbca8b150e7e5ad712cf238ec664a97f
Pavel Kharytonau <58185330+pkmsq@users.noreply.github.com>
2022-07-17 20:30:13 +0300

    Add files via upload

---
+++ frontend/PiKey/theme/Magento_Theme/requirejs-config.js
@@ -0,0 +1,11 @@
+var config = {
+    paths: {
+        "jquery.bootstrap": "Magento_Theme/js/lib/bootstrap.min",
+
+    },
+    shim: {
+        'jquery.bootstrap': {
+            'deps': ['jquery']
+        }
+    }
+};
